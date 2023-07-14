be116f603acf88f09c18f7aab793e14fc387be89
SifaKasena <sifa.collins@gmail.com>
2023-07-14 10:04:13 +0300

    Fix pycodestyle

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+"""Module contains efinition of class Square"""
+
 
 class square():
-    
+    """Square class"""
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """Initialize a square insatance with kwargs"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,10 +18,13 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Get the perimeter of a Square instance """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """Return string representation of a Square instance"""
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
