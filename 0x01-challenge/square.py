fae0a340669af0be4ebdaded2496ed1632d61c9e
SifaKasena <sifa.collins@gmail.com>
2023-07-14 12:04:30 +0300

    Refactor

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -4,31 +4,38 @@ square class
 """
 
 
-class square():
-    """ square class definition """
+class Rectangle:
+    """ Rectangle class definition """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """ Initializes a square instance """
+        """ Initializes a rectangle instance """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
+        """ Area of the rectangle """
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
-        """ Returns the perimeter of a square """
+        """ Returns the perimeter of a rectangle """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ Returs a string representation of a square instance """
+        """ Returs a string representation of a rectangle instance """
         return "{}/{}".format(self.width, self.height)
 
 
-if __name__ == "__main__":
+class square(Rectangle):
+    """ square class definition """
 
+    def __init__(self, *args, **kwargs):
+        """Initializes a square instance"""
+        super().__init__(*args, **kwargs)
+
+    
+if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
