61b3255f9e708a8d1e9f0e72eee3815431005cea
SifaKasena <sifa.collins@gmail.com>
2023-07-14 10:52:47 +0300

    [Fix:] Pycodestyle and documentation

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+"""
+square class
+"""
+
 
 class square():
-    
+    """ square class definition """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ Initializes a square instance """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,10 +19,13 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """ Returns the perimeter of a square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Returs a string representation of a square instance """
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
