e833d73f5c0d5274a8b0a2bade11f37f9f4bac67
SifaKasena <sifa.collins@gmail.com>
2023-07-14 10:27:18 +0300

    Revert to original file

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,39 +1,27 @@
 #!/usr/bin/python3
-"""Module contains efinition of class Square"""
 
-
-class square:
-    """Square class definition"""
-
+class square():
+    
     width = 0
     height = 0
 
+    
     def __init__(self, *args, **kwargs):
-        """Initialize a square insatance with kwargs"""
-        if len(args) == 1:
-            self.width = args[0]
-            self.height = args[0]
-        elif len(args) > 1:
-            self.width = args[0]
-            self.height = args[1]
-        elif kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.height
+        return self.width * self.width
 
     def PermiterOfMySquare(self):
-        """ Get the perimeter of a Square instance """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """Return string representation of a Square instance"""
         return "{}/{}".format(self.width, self.height)
 
-
 if __name__ == "__main__":
+
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
